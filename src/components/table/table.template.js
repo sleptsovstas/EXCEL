05375f0b59348ebea59003f6d5c54d590c63776e
sleptsovstas <sleptsovstas.v@gmail.com>
2022-03-28 16:35:50 +0900

    Finish base table logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,17 +3,25 @@ const CODES = {
   Z: 90
 }
 
-function toCell(_, col) {
-  return `
-    <div class='cell' contenteditable data-col='${col}'></div>
-  `
+function toCell(row) {
+  return function(_, col) {
+    return `
+      <div 
+        class="cell" 
+        contenteditable 
+        data-col="${col}"
+        data-type="cell"
+        data-id="${row}:${col}"
+      ></div>
+    `
+  }
 }
 
 function toColumn(col, index) {
   return `
-    <div class='column' data-type='resizable' data-col='${index}'>
+    <div class="column" data-type="resizable" data-col="${index}">
       ${col}
-      <div class='col-resize' data-resize="col"></div>
+      <div class="col-resize" data-resize="col"></div>
     </div>
   `
 }
@@ -21,12 +29,12 @@ function toColumn(col, index) {
 function createRow(index, content) {
   const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
   return `
-    <div class='row' data-type='resizable'>
-      <div class='row-info'>
+    <div class="row" data-type="resizable">
+      <div class="row-info">
         ${index ? index : ''}
         ${resize}
       </div>
-      <div class='row-data'>${content}</div>
+      <div class="row-data">${content}</div>
     </div>
   `
 }
@@ -36,7 +44,7 @@ function toChar(_, index) {
 }
 
 export function createTable(rowsCount = 15) {
-  const colsCount = CODES.Z - CODES.A + 1
+  const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
   const rows = []
 
   const cols = new Array(colsCount)
@@ -47,12 +55,13 @@ export function createTable(rowsCount = 15) {
 
   rows.push(createRow(null, cols))
 
-  for (let i = 0; i < rowsCount; i++) {
+  for (let row = 0; row < rowsCount; row++) {
     const cells = new Array(colsCount)
         .fill('')
-        .map(toCell)
+        .map(toCell(row))
         .join('')
-    rows.push(createRow(i + 1, cells))
+
+    rows.push(createRow(row + 1, cells))
   }
 
   return rows.join('')
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,20 +1,36 @@
-import { DomListener } from '@core/DomListener'
+import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.enitter = options.enitter
+    this.unsubscribers = []
+
+    this.prepare()
   }
 
-  // Возвращает шаблон компонента
+  prepare() {}
+
   toHTML() {
     return ''
+  }
+
+  $enit(event, ...args) {
+    this.enitter.enit(event, ...args)
+  }
+
+  $on(event, fn) {
+    const unsub = this.enitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
   }
 
   init() {
     this.initDOMListeners()
   }
+
   destroy() {
     this.removeDOMListeners()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
