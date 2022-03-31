eea0cdf7e47635d9d915bf927eb0db9990b12df7
Stanislav <81537969+sleptsovstas@users.noreply.github.com>
2022-03-31 14:57:44 +0900

    Finish redux and table logic (#6)

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,5 +1,5 @@
 import {ExcelComponent} from '@core/ExcelComponent'
-import { $ } from '@core/dom'
+import {$} from '@core/dom'
 
 export class Formula extends ExcelComponent {
   static className = 'excel__formula'
@@ -8,6 +8,7 @@ export class Formula extends ExcelComponent {
     super($root, {
       name: 'Formula',
       listeners: ['input', 'keydown'],
+      subscribe: ['currentText'],
       ...options
     })
   }
@@ -25,23 +26,24 @@ export class Formula extends ExcelComponent {
     this.$formula = this.$root.find('#formula')
 
     this.$on('table:select', $cell => {
-      this.$formula.text($cell.text())
-    })
-
-    this.$on('table:input', $cell => {
-      this.$formula.text($cell.text())
+      this.$formula.text($cell.data.value)
     })
   }
 
+  storeChanged({currentText}) {
+    this.$formula.text(currentText)
+  }
+
   onInput(event) {
-    this.$enit('formula:input', $(event.target).text())
+    const text = $(event.target).text()
+    this.$emit('formula:input', text)
   }
 
   onKeydown(event) {
     const keys = ['Enter', 'Tab']
     if (keys.includes(event.key)) {
       event.preventDefault()
-      this.$enit('formula:done')
+      this.$emit('formula:done')
     }
   }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,31 +4,52 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
-    this.enitter = options.enitter
+    this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
+    this.store = options.store
     this.unsubscribers = []
 
     this.prepare()
   }
 
+  // Настраивааем наш компонент до init
   prepare() {}
 
+  // Возвращает шаблон компонента
   toHTML() {
     return ''
   }
 
-  $enit(event, ...args) {
-    this.enitter.enit(event, ...args)
+  // Уведомляем слушателей про событие event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
   }
 
+  // Подписываемся на событие event
   $on(event, fn) {
-    const unsub = this.enitter.subscribe(event, fn)
+    const unsub = this.emitter.subscribe(event, fn)
     this.unsubscribers.push(unsub)
   }
 
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
+
+  // Сюда приходят только изменения по тем полям, на которые мы подписались
+  storeChanged() {}
+
+  isWatching(key) {
+    return this.subscribe.includes(key)
+  }
+
+  // Инициализируем компонент
+  // Добавляем DOM слушателей
   init() {
     this.initDOMListeners()
   }
 
+  // Удаляем компонент
+  // Чистим слушатели
   destroy() {
     this.removeDOMListeners()
     this.unsubscribers.forEach(unsub => unsub())
